9d351074b89d9056b17d01fa421ea50581b6eb14
Marius Weidner <weidner@cs.uni-freiburg.de>
2021-11-26 17:06:52 +0100

    add 4)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # logical
 Evaluates propositional logical formulas in the form given in the lecture "Logik für Studierende der Informatik WS21" @ uni freiburg
+
+see main.rs for example usage
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     println!("1) {}", Formula::and(Formula::neg(f.clone()), Formula::neg(g.clone())).is_satisfiable());
     println!("2) {}", Formula::and(f.clone(), g.clone()).is_satisfiable());
     println!("3) {}", Formula::or(f.clone(), g.clone()).is_tautology());
+    println!("4) {}", !Formula::and(f.clone(), g.clone()).is_satisfiable());
     println!("5) {}", Formula::implies(Formula::neg(f.clone()), g.clone()).is_tautology());
     println!("6) {}", !Formula::implies(f.clone(), g.clone()).is_tautology());
     println!("7) {}", !Formula::equivalence(f.clone(), g.clone()).is_satisfiable());
